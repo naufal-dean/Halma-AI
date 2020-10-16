a1f27bc4ff3bff0133e76ecd53017f389b5885ff
naufal-dean <naufal.dean@gmail.com>
2020-10-16 20:40:48 +0700

    Add board UI

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-from model import Board, GameState, Player
+from model import *
 
 
 class PageIdx(IntEnum):
@@ -17,7 +17,8 @@ class PageIdx(IntEnum):
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi(os.getcwd() + '/view/main_window.ui', self)
+        loadUi(os.path.join(os.getcwd(), "view", "main_window.ui"), self)
+        self.gameState = None
         # change page helper
         self.changePage = lambda idx: self.stackedWidget.setCurrentIndex(idx)
         # setup ui
@@ -34,10 +35,36 @@ class MainWindow(QMainWindow):
         # in game page
         self.quitGameBtn.clicked.connect(lambda: self.changePage(PageIdx.MAIN_MENU))
 
+    def initBoardUI(self):
+        for r, cellRow in enumerate(self.gameState.board.cells):
+            for c, cell in enumerate(cellRow):
+                button = QPushButton()
+                button.clicked.connect(self.cellClickedHandler)
+                button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+                button.setStyleSheet(self.getCellStyleSheet(cell.cell_type))
+                self.fields.addWidget(button, r, c)
+        self.updatePionPositionUI()
+
+    def updatePionPositionUI(self):
+        for r, cellRow in enumerate(self.gameState.board.cells):
+            for c, cell in enumerate(cellRow):
+                button = self.fields.itemAtPosition(r, c).widget()
+                if cell.pion == Pion.NONE:
+                    button.setIcon(QIcon()); continue;
+                icon_filename = "pion_red.png" if cell.pion == Pion.RED else "pion_green.png"
+                icon_path = os.path.join(os.getcwd(), "resource", "image", icon_filename)
+                button.setIcon(QIcon(icon_path))
+
     # Slot methods
     def startGame(self, humanPlayer: Player, boardSize=16):
         self.changePage(PageIdx.IN_GAME)
         self.initGameState(humanPlayer, boardSize)
+        self.initBoardUI()
+
+    def cellClickedHandler(self):
+        button = self.sender()
+        r, c, _, _ = self.fields.getItemPosition(self.fields.indexOf(button))
+        print(r, c)
 
     # Game methods
     def initGameState(self, humanPlayer, boardSize=16):
@@ -56,8 +83,21 @@ class MainWindow(QMainWindow):
         else:
             message.setIcon(QMessageBox.Information)
         message.setWindowTitle(title)
-        message.setWindowIcon(QIcon("icon/qmessage_icon.png"))
         message.setText(text)
         message.setInformativeText(subtext)
         message.setStandardButtons(QMessageBox.Ok)
         message.exec_()
+
+    def getCellStyleSheet(self, cellType: CellType):
+        bgColor = ("#ef5350" if cellType == CellType.RED_HOUSE else
+                   "#66bb6a" if cellType == CellType.GREEN_HOUSE else
+                   "#bdbdbd")
+        stylesheet = """QPushButton {{
+                            background-color: {bgColor};
+                            border: 1px solid #1b1b1b;
+                            border-radius: 0;
+                        }}
+                        QPushButton:hover {{
+                            border: 2px solid yellow;
+                        }}""".format(bgColor=bgColor)
+        return stylesheet
