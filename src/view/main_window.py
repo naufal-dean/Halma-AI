cb6194b22d92960d37ab2eb63af51fe716e0142a
naufal-dean <naufal.dean@gmail.com>
2020-10-19 12:15:37 +0700

    Bug fix, add end game and quit dialog window, refactor code

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -49,7 +49,7 @@ class MainWindow(QMainWindow):
         self.humanVsMinimax.clicked.connect(lambda:self.setGameMode(GameMode.HUMAN_MINIMAX))
         self.humanVsLocalSearch.clicked.connect(lambda:self.setGameMode(GameMode.HUMAN_LOCAL))
         self.minimaxVsLocalSearch.clicked.connect(lambda:self.setGameMode(GameMode.MINIMAX_LOCAL))
-        
+
         # select side page
         self.pRedBtn.clicked.connect(lambda: self.setCurrentPlayer(Player.RED))
         self.pGreenBtn.clicked.connect(lambda: self.setCurrentPlayer(Player.GREEN))
@@ -57,9 +57,13 @@ class MainWindow(QMainWindow):
 
         # max time page
         self.startGameButton.clicked.connect(lambda: self.startGame(self.currentPlayer, self.boardSize, self.maxTime.value()))
-        
+
         # in game page
-        self.quitGameBtn.clicked.connect(self.quitGame)
+        quitConfirmation = lambda btn: self.quitGame() if btn.text() == "Yes" else None
+        self.quitGameBtn.clicked.connect(lambda: self.spawnDialogWindow("Quit Game",
+                                            "Are you sure you want to quit the game?",
+                                            subtext="Your progress will be discarded",
+                                            callback=quitConfirmation))
 
     def initBoardUI(self):
         for r, cellRow in enumerate(self.gameState.board.cells):
@@ -71,6 +75,7 @@ class MainWindow(QMainWindow):
                 button.setStyleSheet(self.getCellStyleSheet(cell.owner))
                 self.fields.addWidget(button, r, c)
         self.updatePionPositionUI()
+        self.updatePlayerTurnUI()
 
     def updatePionPositionUI(self):
         for r, cellRow in enumerate(self.gameState.board.cells):
@@ -82,19 +87,44 @@ class MainWindow(QMainWindow):
                 icon_path = os.path.join(os.getcwd(), "resource", "image", icon_filename)
                 button.setIcon(QIcon(icon_path))
 
+    def updatePlayerTurnUI(self):
+        if self.gameState.act_player == Player.GREEN:
+            self.curPlayer.setText("GREEN'S TURN")
+        else:
+            self.curPlayer.setText("RED'S TURN")
+
+    def checkWinnerUI(self):
+        winner = self.gameState.check_winner()
+        if winner is not None:
+            def restartOrQuitGame(btn):
+                self.quitGame()
+                if btn.text() == "Yes":
+                    self.startGame(self.currentPlayer, self.boardSize, self.maxTime.value())
+
+            self.spawnDialogWindow("Game Ended", "The Winner is Player " + winner.name,
+                                   subtext="Restart Game?", callback=restartOrQuitGame)
+            return True
+        return False
+
     # Slot methods
     def startGame(self, humanPlayer: Player, boardSize :int, max_time :int):
         self.initGameState(humanPlayer, boardSize, max_time)
         self.initBoardUI()
         self.changePage(PageIdx.IN_GAME)
-        if(self.gameState.act_player == Player.GREEN):
-            self.curPlayer.setText("GREEN'S TURN")
-        else:
-            self.curPlayer.setText("RED'S TURN")
+        # AI move first act_player is not hum_player
+        if self.gameState.act_player != self.gameState.hum_player:
+            # AI move
+            self.calculateAIMove()
+            self.updatePionPositionUI()
+            # check if AI win
+            if self.checkWinnerUI(): return
+            # next turn: human move
+            self.gameState.next_turn()
+            self.updatePlayerTurnUI()
 
     def quitGame(self):
         for idx in reversed(range(self.fields.count())):
-            self.fields.itemAt(idx).widget().deleteLater()
+            self.fields.itemAt(idx).widget().setParent(None)
         self.gameState = None
         self.changePage(PageIdx.MAIN_MENU)
 
@@ -113,32 +143,31 @@ class MainWindow(QMainWindow):
         if self.actCell and (self.gameState.board[self.actCell[0], self.actCell[1]], self.gameState.board[r, c]) in self.legalMoves:  # moving pion
             # move pion
             self.gameState.board.apply_step((self.gameState.board[self.actCell[0], self.actCell[1]], self.gameState.board[r, c]))
-            
             self.updatePionPositionUI()
-            # check winner
-            winner = self.gameState.check_winner()
-            if winner is not None:
-                self.spawnDialogWindow("Game Ended", "The Winner is Player " + winner.name)
-            else:
-                self.gameState.next_turn()
-                self.calculateAIMove()
-                self.updatePionPositionUI()
-                if(self.gameState.act_player == Player.GREEN):
-                    self.curPlayer.setText("GREEN'S TURN")
-                else:
-                    self.curPlayer.setText("RED'S TURN")
-
             # update new active cell and new legal moves
             self.actCell = None
             self.legalMoves = []
+            # check if human win
+            if self.checkWinnerUI(): return
+            # next turn: AI move
+            self.gameState.next_turn()
+            self.updatePlayerTurnUI()
+            # calculate AI move
+            self.calculateAIMove()
+            self.updatePionPositionUI()
+            # check if AI win
+            if self.checkWinnerUI(): return
+            # next turn: human move
+            self.gameState.next_turn()
+            self.updatePlayerTurnUI()
         else:  # selecting pion
             # pion check if existing and owned
             cell = self.gameState.board[r, c]
-            if not cell.occupied_by(self.gameState.act_player):
+            if not cell.occupied_by(self.gameState.hum_player):
                 highlightBtn(button, "none"); self.actCell = None; self.legalMoves = []; return;
             # update new active cell and new legal moves
             self.actCell = (r, c)
-            self.legalMoves = self.gameState.board.legal_moves(r, c, self.gameState.act_player)
+            self.legalMoves = self.gameState.board.legal_moves(r, c, self.gameState.hum_player)
             highlightBtn(button, "yellow")
             for legalMove in self.legalMoves:
                 highlightBtn(self.fields.itemAtPosition(legalMove[1].row, legalMove[1].col).widget(), "red")
@@ -167,7 +196,8 @@ class MainWindow(QMainWindow):
         self.gameState.board.apply_step(step)
 
     # Helper methods
-    def spawnDialogWindow(self, title, text, subtext="", type="Information"):
+    def spawnDialogWindow(self, title, text, yesBtnLbl="Yes", noBtnLbl="No",
+                          subtext="", type="Information", callback=None):
         message = QMessageBox()
         if type == "Question":
             message.setIcon(QMessageBox.Question)
@@ -180,7 +210,9 @@ class MainWindow(QMainWindow):
         message.setWindowTitle(title)
         message.setText(text)
         message.setInformativeText(subtext)
-        message.setStandardButtons(QMessageBox.Ok)
+        message.addButton(yesBtnLbl, QMessageBox.YesRole)
+        message.addButton(noBtnLbl, QMessageBox.NoRole)
+        if callback: message.buttonClicked.connect(callback)
         message.exec_()
 
     def getCellStyleSheet(self, cellType: CellType):
